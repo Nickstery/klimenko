647166d7a71dc57671d3b20b5b2ed2641e48f0f7
ihorklymenko <ihorklim93@gmail.com>
2017-02-14 10:04:51 +0000

    added downloads to "abstracts" bar

--- controller/MainController.php
+++ controller/MainController.php
@@ -182,6 +182,38 @@ class MainController
         readfile('../storage/attachments/announcement.pdf');
     }
 
+    public function abstract_pdf()
+    {
+        $filename = "../storage/attachments/example_en.pdf";    
+        header('Pragma: public');
+	header('Expires: 0');
+	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+	header('Cache-Control: private', false); 
+	header('Content-Type: application/pdf');
+
+	header('Content-Disposition: attachment; filename="'. basename($filename) . '";');
+	header('Content-Transfer-Encoding: binary');
+	header('Content-Length: ' . filesize($filename));
+
+	readfile($filename);
+    }        
+
+    public function abstract_tex()
+    {
+        $filename = "../storage/attachments/example_en.tex";
+        header('Pragma: public');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Cache-Control: private', false);
+        header('Content-Type: application/pdf');
+
+        header('Content-Disposition: attachment; filename="'. basename($filename) . '";');
+        header('Content-Transfer-Encoding: binary');
+        header('Content-Length: ' . filesize($filename));
+
+        readfile($filename);
+    }
+
     public function image($file)
     {
         readfile('../public/images/'.$file);
--- views/abstracts.tpl
+++ views/abstracts.tpl
@@ -4,4 +4,7 @@
     <h2>Abstracts</h2>
 Please use this template to prepare your abstract. Please note that it should not exceed one page.</br>
 
+<a href="http://35.162.114.150/abstract_pdf">Download PDF template</a><br/>
+<a href="http://35.162.114.150/abstract_tex">Download TeX template</a><br/>
+
 {/block}
